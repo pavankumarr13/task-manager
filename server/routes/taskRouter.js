2f68ecfcaea8d8d0c55366122ecb92ab6c7890a3
kyrios12 <saahilhumayun@gmail.com>
2024-06-14 11:58:59 +0530

    CJS modules

--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -1,16 +1,19 @@
-import { Router } from "express";
-import {
-  fetchTasks,
-  getAddTask,
-  postAddTask,
-  getUpdateTask,
-  patchUpdateTask,
-  deleteTask,
-} from "../controllers/taskController.js";
-import verify from "../middleware.js";
+// import { Router } from "express";
+const router = require('express').Router();
+// import {
+//   fetchTasks,
+//   getAddTask,
+//   postAddTask,
+//   getUpdateTask,
+//   patchUpdateTask,
+//   deleteTask,
+// } from "../controllers/taskController.js";
+const {fetchTasks,postAddTask,patchUpdateTask,deleteTask} = require('../controllers/taskController.js');
+const verify = require('../middleware.js');
+// import verify from "../middleware.js";
 // let token = localStorage.getItem
 
-let router = Router();
+// let router = Router();
 
 // Read all tasks
 // Protected route
@@ -30,4 +33,5 @@ router.patch("/updateTask/:id", verify, patchUpdateTask);
 // Delete task
 router.delete("/:id", verify, deleteTask);
 
-export default router;
+// export default router;
+module.exports = router;
