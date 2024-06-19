f2242fcd351666224389b4243e47cbda275b0e10
PavanKumar R <rpavankumar159@gmail.com>
2024-06-19 14:53:41 +0530

    auth update

--- controllers/authController.js
+++ controllers/authController.js
@@ -19,7 +19,7 @@ const registerPost = async (req, res) => {
 
     // changed
     if (userExist) {
-      return res.status(404).json({ message: "User already exist" });
+      return res.status(400).json({ message: "User already exist" });
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = await User.create({
